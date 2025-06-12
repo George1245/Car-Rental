ed03027c7fbda3f3a50204e2795052f3299dcdaa
george x <george20191700199@cis.asu.edu.eg>
2025-06-12 13:00:02 +0300

    Jwt Login Added
    Add JWT authentication and user management features
    
    - Updated `AccountController.cs` with new endpoints for user registration, login
    
    - Configured JWT authentication in `Program.cs` with signing key and validation parameters.

--- WebApplication1/Repsitory/AccountRepository.cs
+++ WebApplication1/Repsitory/AccountRepository.cs
@@ -1,6 +1,13 @@
 ﻿
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Runtime.CompilerServices;
+using System.Security.Claims;
+using System.Text;
 using WebApplication1.DTO;
 using WebApplication1.Models;
 
@@ -14,13 +21,21 @@ namespace WebApplication1.Repsitory
         public RoleManager<IdentityRole> _roleManager;
         private readonly AppDbContext _appDbContext;
         public App_User _user;
-        public AccountRepository(UserManager<App_User> _userManager, SignInManager<App_User> _signInManager, RoleManager<IdentityRole> _roleManager,AppDbContext appDbContext,App_User _user)
+        public IConfiguration Configuration;
+        public AccountRepository(UserManager<App_User> _userManager, SignInManager<App_User> _signInManager, RoleManager<IdentityRole> _roleManager,AppDbContext appDbContext,App_User _user, IConfiguration _Configuration)
         { 
             this._userManager = _userManager;
             this._signInManager = _signInManager;   
             this._roleManager = _roleManager;   
             this._appDbContext = appDbContext;
             this._user =_user ;
+            Configuration=_Configuration;
+        }   
+
+        public App_User getUserByName(string userName)
+        {
+            var user = _appDbContext.Users.FirstOrDefault(U => U.UserName == userName);
+            return user;
         }
         public async Task<bool> Register(userRegisterDTO userRegister)
         {
@@ -48,10 +63,39 @@ namespace WebApplication1.Repsitory
             return false;   
 
         }
-        public Task<bool> LogIn(UserLoginDTO userLogin) 
+        public async Task<string> LogIn(UserLoginDTO userLogin) 
         {
-            return Task.FromResult(true);
+            if (userLogin != null)
+            {
+                App_User User = getUserByName(userLogin.userName);
+                if (User != null)
+                {
+                    if (await _userManager.CheckPasswordAsync(User, userLogin.Password))
+                    {
 
+
+                        //create Token
+                        List<Claim> claims = new List<Claim> {
+                new Claim(ClaimTypes.Name,User.UserName),
+                new Claim(ClaimTypes.MobilePhone,User.PhoneNumber)
+                };
+                        var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration["Keys:JwtKey"]));
+                        var signincredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                        var Token = new JwtSecurityToken(
+                            claims: claims,
+                            expires: DateTime.Now.AddDays(1),
+                            signingCredentials: signincredentials
+                            );
+                        string secretToken = new JwtSecurityTokenHandler().WriteToken(Token);
+                        return secretToken;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            return null;
         }
         public Task<bool> LogOut()
         {
